307a9ff5730a2f9be0d6b9a6ca496a9516bd3ab9
ravikumar-qait <ravikumar@qainfotech.com>
2020-02-06 14:23:46 +0530

    updated code

--- pr1.py
+++ pr1.py
@@ -3,10 +3,14 @@ import shutil
 from os import makedirs
 
 my_list = []
-my_list = os.listdir("/home/qainfotech/Desktop/ravi/new1")
+
+int1 = input("Enter Source path from which you want to move the Files: ")
+
+int2 = input("Enter Destination path to which you want to move the Files: ")
+my_list = os.listdir(int1)
 size = len(my_list)
-print("size is" , size)
-k = int(input("Enter k "))
+print("Total no of files are: " , size)
+k = int(input("Enter the no of folders your want to divide into: "))
 #cnt = 0
 #for files in os.listdir("/home/qainfotech/Desktop/ravi/new1"):
  #   cnt = cnt+1
@@ -16,7 +20,7 @@ if (rem!=0):
 else:
     folder_to_create = int((size/k))
 folder_path = []
-print(folder_to_create)
+#print(folder_to_create)
 for i in range(folder_to_create):
     x = input("give name of folder ")
     makedirs(x)
@@ -28,10 +32,10 @@ y = 0
 for i in range(folder_to_create):
     for j in range(k):
         if(y<size):
-            pth1 = '/home/qainfotech/Desktop/ravi/new1/'+my_list[y]
+            pth1 = int1 +'/'+my_list[y]
             source = pth1;
             y = y+1
-            pth2 = '/home/qainfotech/Desktop/python/' + folder_path[i]
+            pth2 = int2 +'/'+ folder_path[i]
             destination = pth2
             shutil.move(source, destination)
 
