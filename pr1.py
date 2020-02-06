8c297a5d67244f01ac0661484ac3007f0e632a47
ravikumar-qait <ravikumar@qainfotech.com>
2020-02-06 16:59:56 +0530

    updated file

--- pr1.py
+++ pr1.py
@@ -2,42 +2,49 @@ import os
 import shutil 
 from os import makedirs
 
-my_list = []
+files_list  = []
+y=0
 
-int1 = input("Enter Source path from which you want to move the Files: ")
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-int2 = input("Enter Destination path to which you want to move the Files: ")
-my_list = os.listdir(int1)
-size = len(my_list)
-print("Total no of files are: " , size)
-k = int(input("Enter the no of folders your want to divide into: "))
-#cnt = 0
-#for files in os.listdir("/home/qainfotech/Desktop/ravi/new1"):
- #   cnt = cnt+1
-rem = size%k
-if (rem!=0):
-    folder_to_create = int((size/k)+1)
+files_list = os.listdir(dir_path)
+total_files = len(files_list)
+
+print("Total no of files are: " ,total_files )
+
+folder_to_divide = int(input("Enter the no of folders your want to divide into:"))
+
+remainder = total_files%folder_to_divide
+if (remainder!=0):
+    folder_to_create = int((total_files/folder_to_divide)+1)
 else:
-    folder_to_create = int((size/k))
-folder_path = []
-#print(folder_to_create)
-for i in range(folder_to_create):
-    x = input("give name of folder ")
-    makedirs(x)
-    folder_path.append(x)
+    folder_to_create = int((total_files/folder_to_divide))
+folder_list = []
 
-#for i in range(len(folder_path)):
-#    print(folder_path[i])
-y = 0
 for i in range(folder_to_create):
-    for j in range(k):
-        if(y<size):
-            pth1 = int1 +'/'+my_list[y]
-            source = pth1;
+    folder_name = input("Give name of folder:")
+    folder_path = dir_path +'/'+ folder_name
+    os.mkdir(folder_path)
+    folder_list.append(folder_name)
+
+
+for i in range(folder_to_create):
+    for j in range(folder_to_divide):
+        if(y<total_files):
+            source_path = dir_path +'/'+files_list[y]
+            source = source_path
+            destination_path = dir_path +'/'+ folder_list[i]
+            destination = destination_path
+            shutil.move(source_path, destination_path)
             y = y+1
-            pth2 = int2 +'/'+ folder_path[i]
-            destination = pth2
-            shutil.move(source, destination)
+
+
+print("Successfully Moved!")
+
+
+
+
+
 
 
 
