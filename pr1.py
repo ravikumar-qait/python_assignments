f67982728c6539c183b0308d04620a042629c381
Ankit Kumar Singh <ankitkumarsingh@qainfotech.com>
2020-02-06 14:15:53 +0530

    Review: first pass

--- pr1.py
+++ pr1.py
@@ -1,22 +1,26 @@
+# ankitksr: Does not run on my system.
 import os
 import shutil 
 from os import makedirs
 
 my_list = []
+# ankitksr: Do not use hard-coded absolute paths. Use relative paths instead.
 my_list = os.listdir("/home/qainfotech/Desktop/ravi/new1")
 size = len(my_list)
 print("size is" , size)
+# ankitksr: What does 'k' symbolise? Use verbose naming conventions.
 k = int(input("Enter k "))
+# ankitksr: Do not put stray comments into final submission.
 #cnt = 0
 #for files in os.listdir("/home/qainfotech/Desktop/ravi/new1"):
  #   cnt = cnt+1
-rem = size%k
+rem = size%k   # ankitksr: What is 'rem'? 
 if (rem!=0):
     folder_to_create = int((size/k)+1)
 else:
     folder_to_create = int((size/k))
 folder_path = []
-print(folder_to_create)
+print(folder_to_create)   # ankitksr: What is the significance of the stray print statement?
 for i in range(folder_to_create):
     x = input("give name of folder ")
     makedirs(x)
