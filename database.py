58a11f4f612efdb66af4b7fa63dd7d99c0092f07
Ankit Kumar Singh <ankitkumarsingh@qainfotech.com>
2020-02-25 14:41:35 +0530

    Review - pass 1

--- database.py
+++ database.py
@@ -7,12 +7,18 @@ mydb = mysql.connector.connect(
   passwd="119181229Rk##",
   database = "mydatabase"
 )
+# ankitksr: Never put stray print statements in final code submission.
 print("connected")
 
 mycursor = mydb.cursor()
 
+# ankitksr:
+# What does 'posts10' denote? If it is a variable name, think about renaming it to something more relevant.
 mycursor.execute("CREATE TABLE posts10 (userId int, id int, title VARCHAR(255), body VARCHAR(255))")
 
+# ankitksr:
+# Never use hardcoded paths in your code. As a result, no one else can run your script.
+# Re-implement using 'relative paths'.
 with open('/home/qainfotech/Desktop/json_data/my.json') as handle:
     mylist = json.loads(handle.read())
 
@@ -32,6 +38,8 @@ mycursor.execute("SELECT * FROM posts10")
 
 myresult = mycursor.fetchall()
 
+# ankitksr:
+# Please print the final result with proper formatting.
 for x in myresult:
   print(x)
 
