3ee137df39baed2091790024683229ed7a85d5dc
ravikumar-qait <ravikumar@qainfotech.com>
2020-02-28 16:44:38 +0530

    remove redundant code

--- projects/p3/prog2.py
+++
@@ -1,8 +0,0 @@
-string_input = input()
-count = 0
-for i in range(len(string_input)-1):
-    char = string_input[i]
-    if char == ' ' and  string_input[i+1] != ' ':
-        count+=1
-
-print(count+1)
--- projects/p3/prog3.py
+++
@@ -1,7 +0,0 @@
-import string
-string_input = input()
-count = 0
-for i in string_input:
-    if i =='.':
-        count +=1
-print(count)
